5ffce1e7c88a16c1d4ad34b5dc753d1a942c41b9
JAWADAHMAD-18 <jawadahmad5618@gmail.com>
2025-10-12 13:59:33 +0500

    routes code on way

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.send("hello"));
+// routes
+import userRoutes from "./routes/user.routes.js";
+
+app.use("/api/v1/user", userRoutes);
 
 export default app
