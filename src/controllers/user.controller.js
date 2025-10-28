aec2222d8e500af3bc8710e5050a3136d089c168
JAWADAHMAD-18 <jawadahmad5618@gmail.com>
2025-10-28 12:27:55 +0500

    feat: add getWatchHistory endpoint and enhance user channel profile retrieval

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.models.js";
 import cordinaryUploadImage from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessandrefreshToken = async (userid) => {
   try {
@@ -263,28 +264,81 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
       $addFields: {
         subscriberCount: { $size: "$subscriber" },
         subscribedtoCount: { $size: "$subscribedto" },
-        isSubscribed:{
-          $cond:{
-            if:{$in:[req.user?._id,"$subscribers.subscriber"]},
-            then:true,
-            else:false
-          }
-        }
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
       },
-    },{
-      $project:{
-        fullname:1,
-        username:1,
-        avatar:1,
-        coverImage:1,
-        subscriberCount:1,
-        subscribedtoCount:1,
-        isSubscribed:1
-      }
-    }
+    },
+    {
+      $project: {
+        fullname: 1,
+        username: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscriberCount: 1,
+        subscribedtoCount: 1,
+        isSubscribed: 1,
+      },
+    },
   ]);
-  if(!channel?.length) throw new ApiError(404, "Channel data  not found error during aggregate ");
-  return res.status(200).json(new ApiResponse(200, channel[0], "Channel data found and send  successfully"));
+  if (!channel?.length)
+    throw new ApiError(404, "Channel data  not found error during aggregate ");
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        channel[0],
+        "Channel data found and send  successfully"
+      )
+    );
+});
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },{
+            $addFields:{//this portion is just for frontend developer to get easier he will get a object now with onkly that vallues warna usay aray jana tha 
+              owner:{
+                $first:"$owner",//yahan par first sy matlab aarray ka first element hy 
+              }
+            }
+          }
+        ],
+      },
+    },
+  ]);
+  res.ststus(200).json(new ApiResponse(200, user[0].watchHistory, "Watch history found"));
 });
 export {
   userRegister,
@@ -297,4 +351,5 @@ export {
   updateUserAvatar,
   updateUserCoverPhoto,
   getUserChannelProfile,
+  getWatchHistory,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logout, userRegister,newRefreshToken } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logout,
+  userRegister,
+  newRefreshToken,
+  changeCurrentPassword,
+  updateUserDetails,
+  updateUserAvatar,
+  updateUserCoverPhoto,
+  getUserChannelProfile,
+  getWatchHistory,
+  getCurrentUser
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/cloudniary.middlewares.js";
 import verifyAuth from "../middlewares/auth.middleware.js";
 
@@ -14,7 +26,17 @@ router.route("/register").post(
 );
 router.route("/login").post(loginUser);
 //secured routes
-router.route("/logout").post(verifyAuth,logout);
+router.route("/logout").post(verifyAuth, logout);
 router.route("/newRefreshToken").post(newRefreshToken);
-
+router.route("/passworf-changed").post(verifyAuth, changeCurrentPassword);
+router.route("/profile-update").patch(verifyAuth, updateUserDetails);//patch usekiya hy takay jo detail user change karay sirf wohi hn 
+router
+  .route("/update_avatar")
+  .patch(verifyAuth, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/updateUserCoverPhoto")
+  .patch(verifyAuth, upload.single("coverimage"), updateUserCoverPhoto);
+router.route("/channel/:username").post(verifyAuth, getUserChannelProfile);
+router.route("/watchHistory").get(verifyAuth, getWatchHistory);
+router.route("/current-user").get(verifyAuth, getCurrentUser);
 export default router;
